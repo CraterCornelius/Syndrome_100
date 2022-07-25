5f8b35ff09cef32f1a518107f803c9344a804876
CraterCornelius <la.trompette.des.morts@gmail.com>
2022-07-25 11:50:56 +0300

    Verify test phase updated

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findNextSeasonMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 14, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 14;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findNewSeasonMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 9, 2, 3, 4, 5, 6, 7, 8, 7, 1};
+        long expected = 9;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
